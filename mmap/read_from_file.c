e57142558fb036a656b63c9b3dbf0a8fbb441b93
Rumen Zlatkov <nozzller@gmail.com>
2021-05-13 19:59:16 +0300

    PID screenshot added

--- mmap/read_from_file.c
+++ mmap/read_from_file.c
@@ -24,6 +24,14 @@ int main(int argc, char **argv)
 
 	file_data = (char *) mmap(0,file_info.st_size, PROT_READ, MAP_SHARED, f, 0);
 
+	printf("PID=%d\n", getpid());
+        for(int i=0; i<file_info.st_size; i++)
+        {
+                printf("%p\n", file_data+i);
+        }
+
+        getchar(); //getchar used so the program cannot stop after completion
+
 	fwrite(file_data, file_info.st_size, sizeof(char), stdout);
 	return 0;
 }
